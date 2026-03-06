3164c8fbdb77b6e5b8ce05a3c30b3e97162319a8
micaellodev <dmicaellozoppi@outlook.com>
2026-03-05 21:22:18 -0500

    feat: Add iOS application icon and generate Tauri build artifacts.

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -7,4 +7,4 @@ import { treaty } from '@elysiajs/eden'
 import type { App } from '../../../wonka-backend/src/index'
 
 // @ts-expect-error Cross-project typescript referencing issue due to multiple node_modules for elysia
-export const api = treaty<App>('http://localhost:3000')
+export const api = treaty<App>('https://wonka-backend-production.up.railway.app')
